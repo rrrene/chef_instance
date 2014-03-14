594b1d763b0bc97e1e3a300d6a045f3da815a6a7
Miah Johnson <miah@chia-pet.org>
2014-03-14 00:02:46 -0700

    reset our namespace. fix a few style issues.

--- lib/resource_instance.rb
+++ lib/resource_instance.rb
@@ -1,49 +1,48 @@
-
 require 'chef/resource'
 
-class Chef
-  class Resource
-    class Instance < Chef::Resource
+module ChefInstance
+  class Instance < Chef::Resource
+    def initialize(new_resource, run_context = nil)
+      super
+      @provider = Chef::Provider::Instance
+      @action = :create
+      @allowed_actions = allowed_actions
+      @created   = false
+      @destroyed = false
+      @disabled  = false
+      @enabled   = false
+      @started   = false
+      @stopped   = false
+    end
 
-      def initialize(new_resource, run_context = nil)
-        super
-        @provider = Chef::Provider::Instance
-        @action = :create
-        @allowed_actions = [:create, :destroy, :enable, :disable, :nothing, :start, :stop, :restart]
-        @created   = false
-        @destroyed = false
-        @disabled  = false
-        @enabled   = false
-        @started   = false
-        @stopped   = false
-      end
+    def created(arg = nil)
+      set_or_return(:created, arg, kind_of: [TrueClass, FalseClass])
+    end
 
-      def created(arg=nil)
-        set_or_return(:created, arg, :kind_of => [TrueClass, FalseClass])
-      end
+    def destroyed(arg = nil)
+      set_or_return(:destroyed, arg, kind_of: [TrueClass, FalseClass])
+    end
 
-      def destroyed(arg=nil)
-        set_or_return(:destroyed, arg, :kind_of => [TrueClass, FalseClass])
-      end
+    def disabled(arg = nil)
+      set_or_return(:disabled, arg, kind_of: [TrueClass, FalseClass])
+    end
 
-      def disabled(arg=nil)
-        set_or_return(:disabled, arg, :kind_of => [TrueClass, FalseClass])
-      end
+    def enabled(arg = nil)
+      set_or_return(:enabled, arg, kind_of: [TrueClass, FalseClass])
+    end
 
-      def enabled(arg=nil)
-        set_or_return(:enabled, arg, :kind_of => [TrueClass, FalseClass])
-      end
+    def started(arg = nil)
+      set_or_return(:started, arg, kind_of: [TrueClass, FalseClass])
+    end
 
-      def started(arg=nil)
-        set_or_return(:started, arg, :kind_of => [TrueClass, FalseClass])
-      end
+    def stopped(arg = nil)
+      set_or_return(:stopped, arg, kind_of: [TrueClass, FalseClass])
+    end
 
-      def stopped(arg=nil)
-        set_or_return(:stopped, arg, :kind_of => [TrueClass, FalseClass])
-      end
+    private
 
+    def allowed_actions
+      [:create, :destroy, :enable, :disable, :nothing, :start, :stop, :restart]
     end
   end
 end
-
-
