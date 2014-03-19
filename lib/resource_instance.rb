00fbfe2804c190e58635271a94265afcdc5e1fd8
Miah Johnson <miah@chia-pet.org>
2014-03-19 16:21:27 -0700

    we have to nest this unless our name is going to be bigger.

--- lib/resource_instance.rb
+++ lib/resource_instance.rb
@@ -1,37 +1,39 @@
 require 'chef/resource'
 
 module ChefInstance
-  # Docs
-  class Instance < Chef::Resource
-    def initialize(new_resource, run_context = nil)
-      super
-      @provider = Chef::Provider::Instance
-      @action = :create
-      @allowed_actions =[:create, :destroy, :enable, :disable, :nothing]
-    end
+  module Resource
+    # Docs
+    class Instance < Chef::Resource
+      def initialize(new_resource, run_context = nil)
+        super
+        @provider = Chef::Provider::Instance
+        @action = :create
+        @allowed_actions = [:create, :destroy, :enable, :disable, :nothing]
+      end
 
-    def install_options(arg = nil)
-      set_or_return(:install_options, arg, kind_of: Hash)
-    end
+      def install_options(arg = nil)
+        set_or_return(:install_options, arg, kind_of: Hash)
+      end
 
-    def install_type(arg = nil)
-      set_or_return(:install_type,
-                    arg,
-                    kind_of: [Symbol],
-                    required: true,
-                    equal_to: [:package])
-    end
+      def install_type(arg = nil)
+        set_or_return(:install_type,
+                      arg,
+                      kind_of: [Symbol],
+                      required: true,
+                      equal_to: [:package])
+      end
 
-    def service_options(arg = nil)
-      set_or_return(:service_options, arg, kind_of: Hash)
-    end
+      def service_options(arg = nil)
+        set_or_return(:service_options, arg, kind_of: Hash)
+      end
 
-    def service_type(arg = nil)
-      set_or_return(:service_type,
-                    arg,
-                    kind_of: Symbol,
-                    required: true,
-                    equal_to: [:runit])
+      def service_type(arg = nil)
+        set_or_return(:service_type,
+                      arg,
+                      kind_of: Symbol,
+                      required: true,
+                      equal_to: [:runit])
+      end
     end
   end
 end
