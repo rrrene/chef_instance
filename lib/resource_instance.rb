289e7fda8ec677539380314243dd6d0095b5b135
Miah Johnson <miah@chia-pet.org>
2014-03-19 10:45:36 -0700

    remove the created/destroyed etc. make this more like every other instance resource ive written.

--- lib/resource_instance.rb
+++ lib/resource_instance.rb
@@ -7,43 +7,31 @@ module ChefInstance
       super
       @provider = Chef::Provider::Instance
       @action = :create
-      @allowed_actions = allowed_actions
-      @created   = false
-      @destroyed = false
-      @disabled  = false
-      @enabled   = false
-      @started   = false
-      @stopped   = false
+      @allowed_actions =[:create, :destroy, :enable, :disable, :nothing]
     end
 
-    def created(arg = nil)
-      set_or_return(:created, arg, kind_of: [TrueClass, FalseClass])
+    def install_options(arg = nil)
+      set_or_return(:install_options, arg, kind_of: Hash)
     end
 
-    def destroyed(arg = nil)
-      set_or_return(:destroyed, arg, kind_of: [TrueClass, FalseClass])
+    def install_type(arg = nil)
+      set_or_return(:install_type,
+                    arg,
+                    kind_of: [Symbol],
+                    required: true,
+                    equal_to: [:package])
     end
 
-    def disabled(arg = nil)
-      set_or_return(:disabled, arg, kind_of: [TrueClass, FalseClass])
+    def service_options(arg = nil)
+      set_or_return(:service_options, arg, kind_of: Hash)
     end
 
-    def enabled(arg = nil)
-      set_or_return(:enabled, arg, kind_of: [TrueClass, FalseClass])
-    end
-
-    def started(arg = nil)
-      set_or_return(:started, arg, kind_of: [TrueClass, FalseClass])
-    end
-
-    def stopped(arg = nil)
-      set_or_return(:stopped, arg, kind_of: [TrueClass, FalseClass])
-    end
-
-    private
-
-    def allowed_actions
-      [:create, :destroy, :enable, :disable, :nothing, :start, :stop, :restart]
+    def service_type(arg = nil)
+      set_or_return(:service_type,
+                    arg,
+                    kind_of: Symbol,
+                    required: true,
+                    equal_to: [:runit])
     end
   end
 end
